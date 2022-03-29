821905e6d81c39d8a61df775d5a4fd7e7a465a1c
kirtidalave <dalavekirti@gmail.com>
2022-03-29 12:49:26 +0530

    area added

--- area.py
+++ area.py
@@ -9,18 +9,23 @@ def area_square(x):
 
 
 if __name__=="__main__":
+    while(True):
+        print('''        1. Area of Rectangle
+        2. Perimeter of Rectangle
+        3. Area of Square''')
 
-    n1 = int(input(" number 1: "))
-    n2 = int(input("number 2: "))
-    print("1. Area of Rectangle :")
-    print("2. Perimeter of Rectangle:")
-    print("3. Area of Square :")
-    ch=int(input("enter your choice"))
-    if ch==1:
-        print(area_rect(n1,n2))
-    elif ch==2:
-        print(perimeter_rect(n1,n2))
-    elif ch==3:
-        print(area_square(n1))
-    else:
-        print("Invalid choice")
+
+        ch=int(input("enter your choice"))
+        if ch==1:
+            n1 = int(input("number 1:"))
+            n2 = int(input("number 2:"))
+            print(area_rect(n1,n2))
+        elif ch==2:
+            n1 = int(input("number 1:"))
+            n2 = int(input("number 2:"))
+            print(perimeter_rect(n1,n2))
+        elif ch==3:
+            n1 = int(input("number 1:"))
+            print(area_square(n1))
+        else:
+            print("Invalid choice")
--- test_area.py
+++ test_area.py
@@ -2,16 +2,16 @@ import area
 def test_areaRec():
     x=5
     y=7
-    result=area.arearec(x,y)
+    result=area.area_rect(x,y)
     assert x*y==result
 
 def test_periRec():
     x=5
     y=8
-    result=area.periRec(x,y)
+    result=area.perimeter_rect(x,y)
     assert x+x+y+y==result
 
 def test_areaSquare():
     x=8
-    result=area.areaSquare(x)
+    result=area.area_square(x)
     assert x*x==result
